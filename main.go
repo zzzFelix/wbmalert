e2f980cf334988b443b10be5a9077a3f83681ada
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2023-08-23 12:43:35 +0200

    feat: add gewobag

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -13,15 +14,23 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
+var degewo = ""
+var interval = 120
+var initialized = false
+
 func main() {
 	for {
 		checkWBM()
-		time.Sleep(240 * time.Second)
+		checkGewobag()
+		fmt.Printf("Going to sleep for %s seconds", strconv.FormatInt(int64(interval), 10))
+		fmt.Println()
+		time.Sleep(time.Duration(interval) * time.Second)
 	}
 }
 
 func checkWBM() {
-	resp, err := http.Get("https://www.wbm.de/wohnungen-berlin/angebote/")
+	link := "https://www.wbm.de/wohnungen-berlin/angebote/"
+	resp, err := http.Get(link)
 	if err != nil {
 		fmt.Println("Es ist ein Fehler aufgetreten")
 	}
@@ -29,11 +38,28 @@ func checkWBM() {
 	body, err := io.ReadAll(resp.Body)
 	content := string(body[:])
 	if reference := "LEIDER HABEN WIR DERZEIT KEINE VERFÜGBAREN WOHNUNGSANGEBOTE"; strings.Contains(content, reference) {
-		fmt.Println("Keine Ergebnisse")
+		fmt.Println("WBM: Keine Ergebnisse")
 	} else if strings.Contains(content, "403 Zugriff verweigert") {
-		fmt.Println("403 Zugriff verweigert")
+		fmt.Println("WBM: 403 Zugriff verweigert")
 	} else {
-		fmt.Println("Neue Ergebnisse!!!!")
+		fmt.Println("WBM: Neue Ergebnisse!!!! " + link)
+		playSound()
+	}
+}
+
+func checkGewobag() {
+	link := "https://www.gewobag.de/fuer-mieter-und-mietinteressenten/mietangebote/?bezirke_all=1&bezirke%5B%5D=charlottenburg-wilmersdorf&bezirke%5B%5D=charlottenburg-wilmersdorf-charlottenburg&bezirke%5B%5D=friedrichshain-kreuzberg&bezirke%5B%5D=friedrichshain-kreuzberg-friedrichshain&bezirke%5B%5D=friedrichshain-kreuzberg-kreuzberg&bezirke%5B%5D=lichtenberg&bezirke%5B%5D=lichtenberg-alt-hohenschoenhausen&bezirke%5B%5D=lichtenberg-falkenberg&bezirke%5B%5D=lichtenberg-fennpfuhl&bezirke%5B%5D=lichtenberg-friedrichsfelde&bezirke%5B%5D=marzahn-hellersdorf&bezirke%5B%5D=marzahn-hellersdorf-marzahn&bezirke%5B%5D=mitte&bezirke%5B%5D=mitte-gesundbrunnen&bezirke%5B%5D=neukoelln&bezirke%5B%5D=neukoelln-buckow&bezirke%5B%5D=neukoelln-rudow&bezirke%5B%5D=pankow&bezirke%5B%5D=pankow-prenzlauer-berg&bezirke%5B%5D=pankow-weissensee&bezirke%5B%5D=reinickendorf&bezirke%5B%5D=reinickendorf-hermsdorf&bezirke%5B%5D=reinickendorf-tegel&bezirke%5B%5D=reinickendorf-waidmannslust&bezirke%5B%5D=spandau&bezirke%5B%5D=spandau-falkenhagener-feld&bezirke%5B%5D=spandau-hakenfelde&bezirke%5B%5D=spandau-haselhorst&bezirke%5B%5D=spandau-staaken&bezirke%5B%5D=spandau-wilhelmstadt&bezirke%5B%5D=steglitz-zehlendorf&bezirke%5B%5D=steglitz-zehlendorf-lichterfelde&bezirke%5B%5D=steglitz-zehlendorf-zehlendorf&bezirke%5B%5D=tempelhof-schoeneberg&bezirke%5B%5D=tempelhof-schoeneberg-lichtenrade&bezirke%5B%5D=tempelhof-schoeneberg-mariendorf&bezirke%5B%5D=tempelhof-schoeneberg-marienfelde&bezirke%5B%5D=tempelhof-schoeneberg-schoeneberg&bezirke%5B%5D=treptow-koepenick&bezirke%5B%5D=treptow-koepenick-niederschoeneweide&bezirke%5B%5D=treptow-koepenick-oberschoeneweide&objekttyp%5B%5D=wohnung&gesamtmiete_von=&gesamtmiete_bis=&gesamtflaeche_von=&gesamtflaeche_bis=&zimmer_von=&zimmer_bis=&sort-by=recent"
+	resp, err := http.Get(link)
+	if err != nil {
+		fmt.Println("Es ist ein Fehler aufgetreten")
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	content := string(body[:])
+	if reference := "Es konnten keine passenden Angebote gefunden werden!"; strings.Contains(content, reference) {
+		fmt.Println("GEWOBAG: Keine Ergebnisse")
+	} else {
+		fmt.Println("GEWOBAG: Neue Ergebnisse!!!! " + link)
 		playSound()
 	}
 }
@@ -45,11 +71,16 @@ func playSound() {
 	}
 
 	streamer, format, err := mp3.Decode(f)
-	if err != nil {
-		fmt.Println(err)
-	}
+
 	defer streamer.Close()
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	if !initialized {
+		err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+		if err != nil {
+			fmt.Println(err)
+		}
+		initialized = true
+	}
+
 	done := make(chan bool)
 	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
 		done <- true
