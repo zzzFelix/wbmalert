a961b739bd6724c80e67fcbe77355848abf6f6ea
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2023-08-24 16:32:30 +0200

    feat: generalize diff check

--- go.mod
+++ go.mod
@@ -4,11 +4,21 @@ go 1.21.0
 
 require (
 	github.com/faiface/beep v1.1.0
+	golang.org/x/net v0.14.0
+)
+
+require (
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+)
+
+require (
 	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
 	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.25
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
 	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
 	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
-	golang.org/x/sys v0.0.0-20190626150813-e07cf5db2756 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,75 +8,103 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
+	"github.com/microcosm-cc/bluemonday"
 )
 
 // Interval in seconds
-var interval = 240
+var interval = 120
+var websites = []Website{
+	{"WBM", "https://www.wbm.de/wohnungen-berlin/angebote/", ""},
+	{"Gewobag", "https://www.gewobag.de/fuer-mieter-und-mietinteressenten/mietangebote/?bezirke%5B%5D=charlottenburg-wilmersdorf-charlottenburg&bezirke%5B%5D=friedrichshain-kreuzberg&bezirke%5B%5D=friedrichshain-kreuzberg-friedrichshain&bezirke%5B%5D=friedrichshain-kreuzberg-kreuzberg&bezirke%5B%5D=pankow&bezirke%5B%5D=pankow-prenzlauer-berg&bezirke%5B%5D=tempelhof-schoeneberg-schoeneberg&objekttyp%5B%5D=wohnung&gesamtmiete_von=&gesamtmiete_bis=&gesamtflaeche_von=&gesamtflaeche_bis=&zimmer_von=&zimmer_bis=&keinwbs=1&sort-by=recent/", ""},
+}
 
-// Do not change
-var initialized = false
-var gewobag = ""
-var wbm = ""
+type Website struct {
+	name     string
+	url      string
+	snapshot string
+}
 
 func main() {
+	initializeWebsites()
+
 	for {
-		checkWBM()
-		checkGewobag()
-		fmt.Printf("Going to sleep for %s seconds", strconv.FormatInt(int64(interval), 10))
-		fmt.Println()
-		time.Sleep(time.Duration(interval) * time.Second)
+		for i := 0; i < len(websites); i++ {
+			websites[i] = checkWebsite(websites[i])
+		}
+		goToSleep()
 	}
 }
 
-func checkWBM() {
-	link := "https://www.wbm.de/wohnungen-berlin/angebote/"
-	resp, err := http.Get(link)
+func initializeWebsites() {
+	for i := 0; i < len(websites); i++ {
+		fmt.Println("Created initial snapshot for " + websites[i].name)
+		websites[i] = createInitialSnapshot(websites[i])
+	}
+	goToSleep()
+}
+
+func createInitialSnapshot(website Website) Website {
+	content := getWebsiteAsString(website)
+	website.snapshot = content
+	return website
+}
+
+func getWebsiteAsString(website Website) string {
+	resp, err := http.Get(website.url)
 	if err != nil {
-		fmt.Println("Es ist ein Fehler aufgetreten")
+		fmt.Println(err)
+		fmt.Println("An error occurred! The website could not be reached!")
+		return "Error"
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	content := string(body[:])
-	if wbm != content {
-		if reference := "LEIDER HABEN WIR DERZEIT KEINE VERFÜGBAREN WOHNUNGSANGEBOTE"; strings.Contains(content, reference) {
-			fmt.Println("WBM: Keine Ergebnisse")
-		} else if strings.Contains(content, "403 Zugriff verweigert") {
-			fmt.Println("WBM: 403 Zugriff verweigert")
-		} else {
-			fmt.Println("WBM: Neue Ergebnisse!!!! " + link)
-			playSound()
-		}
-		wbm = content
-	} else {
-		fmt.Println("WBM: Ergebnisse unverändert")
-	}
+	content = sanitizeHtml(content)
+	content = removeAllWhitespace(content)
+	return content
 }
 
-func checkGewobag() {
-	link := "https://www.gewobag.de/fuer-mieter-und-mietinteressenten/mietangebote/?bezirke%5B%5D=charlottenburg-wilmersdorf-charlottenburg&bezirke%5B%5D=friedrichshain-kreuzberg&bezirke%5B%5D=friedrichshain-kreuzberg-friedrichshain&bezirke%5B%5D=friedrichshain-kreuzberg-kreuzberg&bezirke%5B%5D=pankow&bezirke%5B%5D=pankow-prenzlauer-berg&bezirke%5B%5D=tempelhof-schoeneberg-schoeneberg&objekttyp%5B%5D=wohnung&gesamtmiete_von=&gesamtmiete_bis=&gesamtflaeche_von=&gesamtflaeche_bis=&zimmer_von=&zimmer_bis=&keinwbs=1&sort-by=recent"
-	resp, err := http.Get(link)
-	if err != nil {
-		fmt.Println("Es ist ein Fehler aufgetreten")
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	content := string(body[:])
-	if gewobag != content {
-		if reference := "Es konnten keine passenden Angebote gefunden werden!"; strings.Contains(content, reference) {
-			fmt.Println("GEWOBAG: Keine Ergebnisse")
-		} else {
-			fmt.Println("GEWOBAG: Neue Ergebnisse!!!! " + link)
-			playSound()
+func removeAllWhitespace(str string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
 		}
-		gewobag = content
-	} else {
-		fmt.Println("GEWOBAG: Ergebnisse unverändert")
-	}
+		return r
+	}, str)
 }
+
+func checkWebsite(website Website) Website {
+	content := getWebsiteAsString(website)
+	if website.snapshot != content {
+		fmt.Println("========= " + website.name + " =========")
+		fmt.Println("Content changed: " + website.url)
+		fmt.Println("====================" + strings.Repeat("=", len(website.name)))
+		playSound()
+		website.snapshot = content
+	} else {
+		fmt.Println("No changes for " + website.name)
+	}
+	return website
+}
+
+func goToSleep() {
+	fmt.Printf("Going to sleep for %s seconds", strconv.FormatInt(int64(interval), 10))
+	fmt.Println()
+	time.Sleep(time.Duration(interval) * time.Second)
+}
+
+func sanitizeHtml(s string) string {
+	p := bluemonday.StripTagsPolicy()
+	html := p.Sanitize(s)
+	return html
+}
+
+var soundInitialized = false
 
 func playSound() {
 	f, err := os.Open("success.mp3")
@@ -87,12 +115,12 @@ func playSound() {
 	streamer, format, err := mp3.Decode(f)
 
 	defer streamer.Close()
-	if !initialized {
+	if !soundInitialized {
 		err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 		if err != nil {
 			fmt.Println(err)
 		}
-		initialized = true
+		soundInitialized = true
 	}
 
 	done := make(chan bool)
