acacba8375a4f58831e66c9f075cf482d5c4f968
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2023-09-21 00:55:26 +0200

    feat: external configuration file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -17,19 +18,23 @@ import (
 	"github.com/microcosm-cc/bluemonday"
 )
 
-var interval = 45 // in seconds
-var websites = []Website{
-	{"WBM", "https://www.wbm.de/wohnungen-berlin/angebote/", ""},
+var interval = 0 // in seconds
+var websites = []Website{}
+
+type Configuration struct {
+	Interval int
+	Websites []Website
 }
 
 type Website struct {
-	name     string
-	url      string
-	snapshot string
+	Name     string
+	Url      string
+	Snapshot string
 }
 
 func main() {
-	initializeWebsites()
+	configuration := readConfiguration()
+	initializeWebsites(configuration)
 
 	for {
 		for i := 0; i < len(websites); i++ {
@@ -39,10 +44,26 @@ func main() {
 	}
 }
 
-func initializeWebsites() {
-	for i := 0; i < len(websites); i++ {
+func readConfiguration() Configuration {
+	jsonFile, err := os.Open("configuration.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	var configuration Configuration
+	json.Unmarshal(byteValue, &configuration)
+
+	defer jsonFile.Close()
+	return configuration
+}
+
+func initializeWebsites(configuration Configuration) {
+	websites = configuration.Websites
+	interval = configuration.Interval
+	for i := 0; i < len(configuration.Websites); i++ {
 		websites[i] = createInitialSnapshot(websites[i])
-		fmt.Println("Created initial snapshot for " + websites[i].name)
+		fmt.Println("Created initial snapshot for " + websites[i].Name)
 	}
 	goToSleep()
 }
@@ -50,13 +71,13 @@ func initializeWebsites() {
 func createInitialSnapshot(website Website) Website {
 	content, error := getWebsiteAsString(website)
 	if error == nil {
-		website.snapshot = content
+		website.Snapshot = content
 	}
 	return website
 }
 
 func getWebsiteAsString(website Website) (string, error) {
-	resp, err := http.Get(website.url)
+	resp, err := http.Get(website.Url)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("An error occurred! The website could not be reached!")
@@ -84,14 +105,14 @@ func checkWebsite(website Website) Website {
 	if error != nil {
 		return website
 	}
-	if website.snapshot != content {
-		fmt.Println("========= " + website.name + " =========")
-		fmt.Println("Content changed: " + website.url)
-		fmt.Println("====================" + strings.Repeat("=", len(website.name)))
+	if website.Snapshot != content {
+		fmt.Println("========= " + website.Name + " =========")
+		fmt.Println("Content changed: " + website.Url)
+		fmt.Println("====================" + strings.Repeat("=", len(website.Name)))
 		playSound()
-		website.snapshot = content
+		website.Snapshot = content
 	} else {
-		fmt.Println("No changes for " + website.name)
+		fmt.Println("No changes for " + website.Name)
 	}
 
 	return website
